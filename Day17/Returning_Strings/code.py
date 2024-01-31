4888f50e912e8e497b05b340e0c06853db1a28b6
unknown <finanoviantika73@gmail.com>
2024-01-31 15:43:39 +0700

    Upload challenge day 17 from 100 days

---
+++ Day17/Returning_Strings/code.py
@@ -0,0 +1,14 @@
+'''
+Link: https://www.codewars.com/kata/55a70521798b14d4750000a4/
+
+Questions:
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+'''
+
+def greet(name):
+    #Good Luck (like you need it)
+    return "Hello, {} how are you doing today?".format(name)
