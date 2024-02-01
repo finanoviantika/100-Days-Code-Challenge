1347cb101c3ee291ef5c34a3bdd95f745a43f86a
unknown <finanoviantika73@gmail.com>
2024-02-01 15:51:22 +0700

    Upload challenge day 18 from 100 days

---
+++ Day18/Find_numbers_which_are_divisible_by_given_number/code.py
@@ -0,0 +1,14 @@
+'''
+Link: https://www.codewars.com/kata/55edaba99da3a9c84000003b/
+
+Questions:
+
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+
+Example(Input1, Input2 --> Output)
+[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+
+'''
+
+def divisible_by(numbers, divisor):
+    return [num for num in numbers if num % divisor == 0]
