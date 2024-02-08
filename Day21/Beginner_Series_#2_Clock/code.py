8209c58247ba8bf1ac59b2fbfe41621b7f936319
unknown <finanoviantika73@gmail.com>
2024-02-08 16:10:13 +0700

    Upload challenge day 21 from 100 days

---
+++ Day21/Beginner_Series_#2_Clock/code.py
@@ -0,0 +1,26 @@
+'''
+Link: https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/
+
+Questions:
+
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+Input constraints:
+
+0 <= h <= 23
+0 <= m <= 59
+0 <= s <= 59
+
+'''
+
+def past(h, m, s):
+    # Good Luck!
+    return (h*3600000)+(m*60000)+(s*1000)
