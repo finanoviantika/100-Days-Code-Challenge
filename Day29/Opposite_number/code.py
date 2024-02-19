7c0615aafbdd7e404bfcbd8128043c6a0c2c48a9
unknown <finanoviantika73@gmail.com>
2024-02-19 15:42:28 +0700

    Upload challenge day 29 from 100 days

---
+++ Day29/Opposite_number/code.py
@@ -0,0 +1,18 @@
+'''
+Link: https://www.codewars.com/kata/56dec885c54a926dcd001095/train/
+
+Questions:
+
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+'''
+
+def opposite(number):
+  # your solution here
+    return -number
