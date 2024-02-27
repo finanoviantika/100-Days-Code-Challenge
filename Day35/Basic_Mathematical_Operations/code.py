faccb9514599a61379dc4c608cddc5957d08652a
unknown <finanoviantika73@gmail.com>
2024-02-27 21:34:02 +0700

    Upload challenge day 35 from 100 days

---
+++ Day35/Basic_Mathematical_Operations/code.py
@@ -0,0 +1,33 @@
+'''
+Link: https://www.codewars.com/kata/57356c55867b9b7a60000bd7/
+
+Questions:
+
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+
+'''
+
+def basic_op(operator, value1, value2):
+    #your code here
+    if operator == "+":
+        return value1 + value2
+    elif operator == "-":
+        return value1 - value2
+    elif operator == "*":
+        return value1 * value2
+    elif operator == "/":
+        if value2 != 0:
+            return value1/value2
+        else:
+            return "Can't divide by 0!"
+    else:
+        return "Error"
