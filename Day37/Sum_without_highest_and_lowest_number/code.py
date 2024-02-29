ac8c815d930aa3c06ba7148c2619f6a6d2f301b4
unknown <finanoviantika73@gmail.com>
2024-02-29 12:00:23 +0700

    Upload challenge day 37 from 100 days

---
+++ Day37/Sum_without_highest_and_lowest_number/code.py
@@ -0,0 +1,25 @@
+'''
+Link: https://www.codewars.com/kata/576b93db1129fcf2200001e6/
+
+Questions:
+
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+Input validation
+If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+'''
+
+def sum_array(arr):
+    #your code here
+    if arr == None or len(arr) < 0:
+        return 0
+    else:
+        return sum(sorted(arr)[1:-1])
