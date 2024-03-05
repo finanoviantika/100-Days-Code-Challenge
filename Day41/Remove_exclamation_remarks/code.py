b4936fea84d951615fa1ac37083a8bf149d56283
unknown <finanoviantika73@gmail.com>
2024-03-05 17:27:09 +0700

    Upload challenge day 41 from 100 days

---
+++ Day41/Remove_exclamation_remarks/code.py
@@ -0,0 +1,13 @@
+'''
+Link: https://www.codewars.com/kata/57a0885cbb9944e24c00008e/
+
+Questions:
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+'''
+
+def remove_exclamation_marks(s):
+    #your code here
+    return s.replace("!", "")
+    
