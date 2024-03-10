2047b70aadfa657e849366ff3f7d0ccc0208e75d
unknown <finanoviantika73@gmail.com>
2024-03-10 18:56:29 +0700

    Upload challenge day 45 from 100 days

---
+++ Day45/Bin_to_Decimal/code.py
@@ -0,0 +1,11 @@
+'''
+Link: https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/train/
+
+Questions: 
+
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+'''
+
+def bin_to_decimal(inp):
+    return int(inp, 2)
